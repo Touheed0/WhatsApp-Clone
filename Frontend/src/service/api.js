6b54ad2a3c7770f6a5a74ecc53f1d6dd17cfc56e
Touheed0 <tuheedahmad360@gmail.com>
2025-10-12 03:17:40 -0700

    Add Live Backend URL

--- Frontend/src/service/api.js
+++ Frontend/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:8000";
+const url = "https://whatsapp-clone-backend-wt80.onrender.com";
 
 export async function AddUser(data) {
     try {
